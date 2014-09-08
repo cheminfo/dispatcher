246a22f9e64f9c0542bab41094fddabbf67079bd
Daniel Kostro <kostro.d@gmail.com>
2014-09-08 13:38:40 +0200

    Change parsed response

--- scheduler/cache.js
+++ scheduler/cache.js
@@ -11,7 +11,7 @@ var cache = exports = module.exports = {
         this.options = options || {};
         data.cfg = _.cloneDeep(config.devices);
         data.status = {};
-        data.param = {};
+        data.entry = {};
         // Every 5 seconds, get status of devices
         var delay= options.delay || 2000;
         var sendEvent=function() {
@@ -25,7 +25,7 @@ var cache = exports = module.exports = {
                         });
                         var id =  data.cfg[i].id;
                         var status = data.status[id] = data.status[id] || {};
-                        data.param[id] = data.param[id] || {};
+                        data.entry[id] = data.entry[id] || {};
 
                         if(entries.length > 1) {
                             debug('Unexpected error..., ', entries.length, 'entries when 1 expected');
@@ -36,10 +36,12 @@ var cache = exports = module.exports = {
                         if(status.active) {
                             status.lastUpdate = entries[0].epoch;
                             status.nbFailures = 0;
-                            data.param[id] = entries[0];
-                            data.param[id] = [];
-                            for(var key in entries[0]) {
-                                    data.param[id].push({
+                            data.entry[id] = {};
+                            data.entry[id].parameters = [];
+                            data.entry[id].epoch = entries[0].epoch;
+                            data.entry[id].deviceId = entries[0].deviceId;
+                            for(var key in entries[0].parameters) {
+                                    data.entry[id].parameters.push({
                                         name: key,
                                         mappedName: data.cfg[i].parameters[key] ? data.cfg[i].parameters[key].name : 'NA',
                                         value: entries[0][key],
@@ -51,10 +53,6 @@ var cache = exports = module.exports = {
                         else {
                             status.nbFailures = status.nbFailures ?  (status.nbFailures+1) : 1;
                         }
-
-
-
-
                     });
                 })(i)
             }
