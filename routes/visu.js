873a08d6eee8034f0473519b8f5d56e90910a463
Daniel Kostro <kostro.d@gmail.com>
2014-09-30 16:13:19 +0200

    Update admin view

--- routes/visu.js
+++ routes/visu.js
@@ -9,31 +9,9 @@ var router = require('express').Router(),
 exports  = module.exports = router;
 
 
-router.get('/configs', function(req, res) {
-    debug('configs');
-    // Get all files in configs
-    var dir = 'configs';
-    var files = getFiles(dir);
-    if(!files) {
-        return res.json(400).json({});
-    }
-    files = files.filter(function(file) {
-        return file.indexOf('.json') > -1;
-    });
+router.get('/configs', getJsonFiles('configs'));
 
-    var result = [];
-    for(var i=0; i<files.length; i++) {
-        result.push({
-            name: files[i],
-            value: {
-                type: 'object',
-                url: path.join('/',dir, files[i])
-            }
-        });
-    }
-
-    return res.status(200).json(result);
-});
+router.get('/devices', getJsonFiles('devices'));
 
 function getFiles(dir) {
     try {
@@ -44,4 +22,29 @@ function getFiles(dir) {
         return null;
     }
 
+}
+
+function getJsonFiles(dir) {
+    return function(req, res) {
+        var files = getFiles(dir);
+        if(!files) {
+            return res.json(400).json({});
+        }
+        files = files.filter(function(file) {
+            return file.indexOf('.json') > -1;
+        });
+
+        var result = [];
+        for(var i=0; i<files.length; i++) {
+            result.push({
+                name: files[i],
+                value: {
+                    type: 'object',
+                    url: path.join('/',dir, files[i])
+                }
+            });
+        }
+
+        return res.status(200).json(result);
+    }
 }
