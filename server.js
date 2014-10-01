47ef43d00099069cee6c7f0d63370841140beba0
Daniel Kostro <kostro.d@gmail.com>
2014-10-01 13:51:08 +0200

    appconfig used to load configuration. serverConfig used only for server configuration, and not editable from admin view

--- server.js
+++ server.js
@@ -76,6 +76,9 @@ http.listen(app.get("port"), app.get("ipaddr"), function() {
 // The root element redirects to the default visualizer view
 var view = '/visualizer/index.html?config=/config/default.json&viewURL=/views/' + defaultView + '.json&dataURL=/data/default.json';
 app.get('/', function(req, res) {
+    res.set({
+        'Cache-Control': 'no-cache'
+    });
     res.redirect(301, view);
 });
 
