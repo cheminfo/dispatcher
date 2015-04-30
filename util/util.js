faf169048626d241f886c1f53a3a7287a9d530cc
Daniel Kostro <kostro.d@gmail.com>
2015-04-30 17:06:33 +0200

    Util: change idStringToNumber

--- util/util.js
+++ util/util.js
@@ -1,9 +1,6 @@
 
 function idStringToNumber(idString) {
-    if(idString[0] === '$' || idString[0] === '%')
         return idString.charCodeAt(0) * 256 + idString.charCodeAt(1);
-    else
-        return idString;
 }
 
 function idNumberToString(idNumber) {
