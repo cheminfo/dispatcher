65652ddb6ef9bd7965c1cc48a4710be2c418ffa8
jul <julien.wist@correounivalle.edu.co>
2015-08-11 18:12:19 -0500

    deepen/flatten semi-flat entries

--- test/filter.js
+++ test/filter.js
@@ -7,15 +7,25 @@ config.addConfiguration('dbtest');
 var filter = new Filter();
 
 describe('Filter functions', function() {
-    it('should flatten entries', function() {
+    it('should flatten a deep entry', function() {
         var f = filter.flattenEntries(deep);
         f.should.eql(flat);
     });
 
-    it('should deepen entries', function() {
+    it('should deepen a flat enty', function() {
         var d = filter.deepenEntries(flat);
         d.should.eql(deep);
-    })
+    });
+
+    it('should deepen a semi-flat entry', function() {
+        var d = filter.deepenEntries(semiFlat);
+        d.should.eql(deep);
+    });
+
+    it('should flatten a semi-flat entry', function() {
+        var f = filter.flattenEntries(semiFlat);
+        f.should.eql(flat);
+    });
 });
 
 var flat = [{
@@ -30,6 +40,15 @@ var deep = [{
     epoch: 0,
     parameters: {
         A: 1,
+        B: 2
+    }
+}];
+
+var semiFlat = [{
+    id: 1,
+    epoch: 0,
+    A: 1,
+    parameters: {
         B: 2
     }
 }];
