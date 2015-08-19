618de660f6e277a7ef37e42605b7aef53bf0c056
stropitek <kostro.d@gmail.com>
2015-08-19 14:38:44 -0500

    Add npm script that converts id string to number

---
+++ scripts/getId.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var util = require('../util/util');
+console.log(util.deviceIdStringToNumber(process.argv[2]));
--- util/util.js
+++ util/util.js
@@ -1,4 +1,6 @@
 function idStringToNumber(idString) {
+    if(idString === undefined) return;
+
     if (idString.length === 2) {
         return idString.charCodeAt(0) * 256 + idString.charCodeAt(1);
     }
