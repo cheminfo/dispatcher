53fe8c0b5e7164f6f47201923b78221716798a13
stropitek <kostro.d@gmail.com>
2015-09-24 00:01:20 +0200

    fix auth: return 401 when no auth given

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ exports.simple = function simple(req, res, next) {
     var auth = req.params.auth || req.query.auth || (req.body ? req.body.auth : null);
     if(!auth) {
         debug('Not passing authentification (no auth given)');
-        return res.json(401);
+        return res.status(401);
     }
 
     var appconfig = config.getAppconfig();
