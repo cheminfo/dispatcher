6f8c608ccbc98f23972d01bc3f75f57868311487
stropitek <kostro.d@gmail.com>
2015-09-24 00:00:07 +0200

    auth: add debug message

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,10 @@ exports = module.exports = {};
 
 exports.simple = function simple(req, res, next) {
     var auth = req.params.auth || req.query.auth || (req.body ? req.body.auth : null);
-    if(!auth) return res.json(401);
+    if(!auth) {
+        debug('Not passing authentification (no auth given)');
+        return res.json(401);
+    }
 
     var appconfig = config.getAppconfig();
     if(appconfig.authKey !== auth) {
