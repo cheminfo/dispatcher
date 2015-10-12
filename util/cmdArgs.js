fcaa1874cc0c9443f0a8b15ea1604aa770056f2e
Daniel Kostro <kostro.d@gmail.com>
2015-10-12 15:59:34 +0200

    autocrlf

--- util/cmdArgs.js
+++ util/cmdArgs.js
@@ -4,6 +4,7 @@ var argv = require('minimist')(process.argv.slice(2));
 var config = require('../configs/config');
 var appconfig = config.getAppconfig();
 
+
 exports = module.exports = function (name, def) {
     var opt = (argv[name]) ? argv[name] : null;
     return opt || appconfig[name] || def;
