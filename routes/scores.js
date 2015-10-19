5affe437681c279aa2dbae4e6abc6aad421820ca
Daniel Kostro <kostro.d@gmail.com>
2015-10-19 13:44:13 +0200

    Score api: return json, optionally as html

--- routes/scores.js
+++ routes/scores.js
@@ -8,16 +8,23 @@ var router = require('express').Router(),
 
 exports = module.exports = router;
 
-router.get('/device/:device', middleware.validateParameters({name: 'device', required: true}), function (req, res) {
+router.get('/device/:device',
+    middleware.validateParameters([
+        { name: 'device', required: true },
+        { name: 'as', required: false }
+    ]), function (req, res) {
     var deviceId = res.locals.parameters.device;
     scores.all(deviceId).then(function (data) {
         console.log(data);
         data.name = deviceId;
-        res.render('scores/scores', {teams: [data]});
+        if(res.locals.parameters.as === 'html') {
+            return res.render('scores/scores', {teams: [data]});
+        }
+        return res.json(data);
     })
 });
 
-router.get('/all', function (req, res) {
+router.get('/all', middleware.validateParameters({name: 'as'}), function (req, res) {
     var devices = config.getMergedDevices();
     var prom = new Array(devices.length);
     for (var i = 0; i < devices.length; i++) {
@@ -27,7 +34,10 @@ router.get('/all', function (req, res) {
         for (var i = 0; i < data.length; i++) {
             data[i].name = devices[i].id;
         }
-        res.render('scores/scores', {teams: data});
+        if (res.locals.parameters.as === 'html') {
+            return res.render('scores/scores', {teams: data});
+        }
+        return res.json(data);
     }, function () {
         res.status(400);
     });
@@ -37,17 +47,17 @@ router.get('/param/:type/:device/:param', middleware.validateParameters({
         name: 'device', required: true
     }),
     function (req, res) {
-        Promise.resolve().then(function() {
-            return scores[req.params.type](res.locals.parameters.device, req.params.param).then(function(score) {
+        Promise.resolve().then(function () {
+            return scores[req.params.type](res.locals.parameters.device, req.params.param).then(function (score) {
                 res.json({
                     points: score,
                     availablePoints: scores.maxPoints(req.params.type)
                 });
             });
-        }).catch(function() {
+        }).catch(function () {
             res.status(400);
         });
 
     }
-)
-;
+);
+
