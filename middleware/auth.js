7efd9f0468389b17973f83bbc14e2cbac94b253f
stropitek <kostro.d@gmail.com>
2015-10-24 14:22:59 +0200

    fix auth response when failing

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,13 +9,13 @@ exports.simple = function simple(req, res, next) {
     var auth = req.params.auth || req.query.auth || (req.body ? req.body.auth : null);
     if(!auth) {
         debug('Not passing authentification (no auth given)');
-        return res.status(401);
+        return res.status(401).json("Unauthorized");
     }
 
     var appconfig = config.getAppconfig();
     if(appconfig.authKey !== auth) {
         debug('Not passing authentification');
-        return res.status(401);
+        return res.status(401).json("Unauthorized");
     }
 
     return next();
